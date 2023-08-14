a5f52816380ea2fcfbbb9cd984c4b3daa9f34fb2
Adway Lachhiramka <66119557+adway7103@users.noreply.github.com>
2023-08-14 22:21:09 +0530

    cc

--- src/Components/Form/TopHeader.jsx
+++ src/Components/Form/TopHeader.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import docsIcon from "/assets/docs.svg";
 const TopHeader = () => {
   return (
-    // <div className="TopHeaderContainer">
-    //   <div className="registrationFormELLipse">
-    //     <img src={docsIcon} />
-    //   </div>
-    // </div>
+    <div className="TopHeaderContainer">
+      <div className="registrationFormELLipse">
+        <img src={docsIcon} />
+      </div>
+    </div>
   );
 };
 
